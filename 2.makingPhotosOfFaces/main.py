4f35ddb272eeb847a3300435d740697f5336f99b
Piotr Opielski <opielski.piotr@gmail.com>
2017-07-02 11:12:46 +0200

    lesson 2 part I making photos of faces

---
+++ 2.makingPhotosOfFaces/main.py
@@ -0,0 +1,23 @@
+import cv2
+
+face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+recognizer = cv2.face.createLBPHFaceRecognizer()
+capture = cv2.VideoCapture(0)
+k = 0
+
+while k != 27:
+    k = cv2.waitKey(30) & 0xff
+    succes,image = capture.read()
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face.detectMultiScale(gray_image, 1.3, 5)
+    counter = 0
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        im_face = image[y:y + h, x:x + w]
+        cv2.imwrite('./faces/' + counter.__str__() + '.jpg',im_face )
+        counter+=1
+    cv2.imshow('img', image)
+
+
+capture.release()
+cv2.destroyAllWindows()
